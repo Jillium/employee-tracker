23fc24c8ef5256500921244588b5e3b9284a5700
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-24 11:23:03 -0700

    employeerole table created

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,15 @@
 DROP TABLE IF EXISTS employeerole;
 DROP TABLE IF EXISTS department;
 
+CREATE TABLE employeerole (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary VARCHAR(30) NOT NULL,
+    department_id INTEGER
+);
+
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
--- CREATE TABLE employeerole (
---     id INTEGER AUTO_INCREMENT PRIMARY KEY,
---     title VARCHAR(30) NOT NULL,
---     salary VARCHAR(30) NOT NULL,
---     department_id INTEGER,
--- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,14 @@
+INSERT INTO employeerole (title, salary, department_id)
+VALUES 
+('Salesperson', '80000', 1),
+('Lead Engineer', '150000', 2),
+('Software Engineer', '120000', 2),
+('Account Manager', '160000', 3),
+('Accountant', '125000', 3),
+('Legal Team Lead', '250000', 4),
+('Lawyer', '190000', 4);
+
+
 INSERT INTO department (department_name)
 VALUES 
 ('Sales'),
@@ -6,12 +17,3 @@ VALUES
 ('Legal');
 
 
--- INSERT INTO employeerole (title, salary, department_id)
--- VALUES 
--- ('Salesperson', '80000', 1),
--- ('Lead Engineer', '150000', 2),
--- ('Software Engineer', '120000', 2),
--- ('Account Manager', '160000', 3),
--- ('Accountant', '125000', 3),
--- ('Legal Team Lead', '250000', 4),
--- ('Lawyer', '190000', 4);
